58525edf943608f429889d73ddced28ba793364a
Lakshmi Baghel <baghellakshmi@gmail.com>
2026-04-10 23:09:39 +0530

    message

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,17 @@
+console.log("Execute line 1");
+console.log("Execute line 2");
+console.log("Execute line 3");
+sumOfNumbers(3,5);
+console.log("Execute line 4");
+console.log("Execute line 5");
+console.log("Execute line 6");
+console.log("Execute line 7");
+console.log("Execute line 8");
+
+
+
+    function sumOfNumbers(a,b)
+    {
+    c=a+b;
+    console.log(c);
+    }
